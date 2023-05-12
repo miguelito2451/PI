80b9a568321bed2662957ff8b83b4b0616c29586
AlexandraMurcia <brigith.murcia17@gmail.com>
2023-05-12 13:01:53 -0500

    Cambios a descatar

--- PI-Food-main/PI-Food-main/api/src/db.js
+++ PI-Food-main/PI-Food-main/api/src/db.js
@@ -31,6 +31,8 @@ sequelize.models = Object.fromEntries(capsEntries);
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
 const { Recipe } = sequelize.models;
+const {Diets} = sequelize.models;
+
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
